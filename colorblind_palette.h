a4a47f3fa9d8b39f23ade55fccb0f478c347e555
Azecy <56807677+Azecy@users.noreply.github.com>
2022-12-12 02:34:16 -0500

    C header for use with SDL2

---
+++ colorblind_palette.h
@@ -0,0 +1,28 @@
+#ifndef COLORBLIND_PALETTE_H
+#define COLORBLIND_PALETTE_H
+
+/*
+Colorblind 16 Palette
+Created by Life Forever Together
+https://lospec.com/palette-list/colorblind-16
+https://github.com/filipworksdev/colorblind-palette-16
+*/
+
+#define CBP_BLACK (SDL_Color){0, 0, 0, 255}
+#define CBP_BLUE (SDL_Color){0, 109, 219, 255}
+#define CBP_BROWN (SDL_Color){143, 78, 0, 255}
+#define CBP_DARK_GRAY (SDL_Color){37, 37, 37, 255}
+#define CBP_DARK_PURPLE (SDL_Color){73, 0, 146, 255}
+#define CBP_EGGPLANT (SDL_Color){23, 23, 35, 255}
+#define CBP_FOREST_GREEN (SDL_Color){0, 73, 73, 255}
+#define CBP_GREEN (SDL_Color){34, 207, 34, 255}
+#define CBP_LIGHT_GRAY (SDL_Color){103, 103, 103, 255}
+#define CBP_LIGHT_PURPLE (SDL_Color){182, 109, 255, 255}
+#define CBP_ORANGE (SDL_Color){219, 109, 0, 255}
+#define CBP_PINK (SDL_Color){255, 109, 182, 255}
+#define CBP_RED (SDL_Color){146, 0, 0, 255}
+#define CBP_TEAL (SDL_Color){0, 153, 153, 255}
+#define CBP_WHITE (SDL_Color){255, 255, 255, 255}
+#define CBP_YELLOW (SDL_Color){255, 223, 77, 255}
+
+#endif
